cbcf5256963a3ceb27586e48529fa69be9a128b6
mdzakariyakhan934-a11y <mdzakariyakhan934@gmail.com>
2026-09-08 23:23:07 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$host = "localhost";
-$user = "your_db_user";     // আপনার ডাটাবেস ইউজারনেম
-$pass = "your_db_password"; // আপনার ডাটাবেস পাসওয়ার্ড
-$dbname = "your_db_name";   // আপনার ডাটাবেসের নাম
+$host = "mysql-2298e29e-mdzakariyakhan934-d309.k.aivencloud.com;
+$user = "your_db_user"avnadmin;     // আপনার ডাটাবেস ইউজারনেম
+$pass = "your_db_password"AVNS_ilLzevUpr3em0mhq4PL; // আপনার ডাটাবেস পাসওয়ার্ড
+$dbname = "your_db_name"defaultdb;   // আপনার ডাটাবেসের নাম
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
