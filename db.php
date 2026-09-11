e34f8df8e6b560c06a9cdfad136a67880ab56777
mdzakariyakhan934-a11y <mdzakariyakhan934@gmail.com>
2026-09-11 03:45:52 -0700

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 
-$host = "mysql-2298e29e-mdzakariyakhan934-d309.k.aivencloud.com;
-$user = "your_db_user"avnadmin;     // আপনার ডাটাবেস ইউজারনেম
-$pass = "your_db_password"AVNS_ilLzevUpr3em0mhq4PL; // আপনার ডাটাবেস পাসওয়ার্ড
-$dbname = "your_db_name"defaultdb;   // আপনার ডাটাবেসের নাম
+$host = "mysql-2298e29e-mdzakariyakhan934-d309.k.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS_ilLzevUpr3em0mhq4PL";
+$dbname = "defaultdb";
+$port = 18480;
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Database Connection Failed: " . $conn->connect_error);
