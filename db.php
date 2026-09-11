6957059f5cf4a0f26597eb4da22b9afe39b4938b
mdzakariyakhan934-a11y <mdzakariyakhan934@gmail.com>
2026-09-11 04:40:17 -0700

    Update db.php

--- db.php
+++ db.php
@@ -5,13 +5,13 @@ $host = "mysql-2298e29e-mdzakariyakhan934-d309.k.aivencloud.com";
 $user = "avnadmin";
 $pass = "AVNS_ilLzevUpr3em0mhq4PL";
 $dbname = "defaultdb";
-$port = 18484; // আপনার Aiven-এর বর্তমান Port
+$port = 18480;
 
 $conn = mysqli_init();
+$conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 10);
 $conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
-$conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 
-if (!$conn->real_connect($host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+if (!$conn->real_connect($host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
 ?>
