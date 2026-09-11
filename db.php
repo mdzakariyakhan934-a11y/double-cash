79ff375f8d6df52aa242e0b8119635e113582fcd
mdzakariyakhan934-a11y <mdzakariyakhan934@gmail.com>
2026-09-11 04:33:32 -0700

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $host = "mysql-2298e29e-mdzakariyakhan934-d309.k.aivencloud.com";
 $user = "avnadmin";
 $pass = "AVNS_ilLzevUpr3em0mhq4PL";
 $dbname = "defaultdb";
-$port = 18480;
+$port = 18484;
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
